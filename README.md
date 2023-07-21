bd2fef5978c011235a630d66b759335f86cc2a3f
rabbyhasan <rabbyhasan4594@gmail.com>
2023-07-22 04:17:07 +0600

    login and register added

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# React + Vite
+# Book My Campus
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
---
+++ src/layout/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Outlet } from 'react-router-dom';
+import NavigationBar from '../Share/NavigationBar/NavigationBar';
+
+
+
+const Main = () => {
+    return (
+        <div>
+            <NavigationBar></NavigationBar>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Main;
